63b973d6d4b5ec59cd51b307e17bf2bfb4bc7a34
harja <harja@192.168.5.13>
2022-03-30 23:52:01 -0400

    made rect and line to test javafx

--- ENGG1420FinalProject/src/engg1420finalproject/ENGG1420FinalProject.java
+++ ENGG1420FinalProject/src/engg1420finalproject/ENGG1420FinalProject.java
@@ -36,15 +36,41 @@ public class ENGG1420FinalProject extends Application {
             circle.setFill(Color.BLUE);
             return circle;
         }
+        
+        public Rectangle createRect(int[] rectData){
+            Rectangle rect = new Rectangle();
+            rect.setX(rectData[0]);
+            rect.setY(rectData[1]);
+            rect.setWidth(rectData[2]);
+            rect.setHeight(rectData[3]);
+            rect.setFill(Color.RED);
+            
+            return rect;
+        }
+        
+        public Line createLine(int[] lineData){
+            Line line = new Line();
+            line.setStartX(lineData[0]);
+            line.setStartY(lineData[1]);
+            line.setEndX(lineData[2]);
+            line.setEndY(lineData[3]);
+            line.setFill(Color.BLACK);
+            
+            return line;
+        }
     @Override
     public void start(Stage primaryStage) {
         int[] circleData =  {200, 150, 50};
+        int[] rectData = {200, 150, 100, 100};
+        int[] lineData = {200, 150, 500, 150};
         
         primaryStage.setTitle("Testing Circle");
         
         Circle circle = createCircle(circleData);
+        Rectangle rect = createRect(rectData);
+        Line line = createLine(lineData);
         
-        Group root = new Group(circle);
+        Group root = new Group(circle, rect, line);
         
         Scene scene = new Scene(root, 400, 300);
         
