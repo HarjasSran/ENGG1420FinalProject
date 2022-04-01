c5640ce8896eaa2856a9f3808120e70181e32b69
harja <harja@192.168.5.13>
2022-03-31 22:55:06 -0400

    animation player class

--- ENGG1420FinalProject/src/engg1420finalproject/AnimationPlayer.java
+++ ENGG1420FinalProject/src/engg1420finalproject/AnimationPlayer.java
@@ -15,50 +15,48 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 public class AnimationPlayer {
     int frames;
     int speed;
     int elements;
+    static Shape[] shapes = {null};
     
     public AnimationPlayer(){
         frames = 1000;
         speed = 10;
         elements = 2;
+        shapes = new Shape[elements];
     }
     
     public AnimationPlayer(int frames, int speed, int elements){
         this.frames = frames;
         this.speed = speed;
         this.elements = elements;
+        shapes = new Shape[elements +1];
     }
     
-    
-    public void loadAnimationFromFile(String file){
+    static public void loadFile(String file){
+        
         try{
             File f = new File(file);
             Scanner s = new Scanner(f);
             while(s.hasNextLine()){
-                //scans the first 3 lines to create an animation player with its 
-                AnimationPlayer player = new AnimationPlayer(getInt(s.nextLine()), getInt(s.nextLine()), getInt(s.nextLine()));
-                String[] shapes = new String[elements + 1];
-                int i = 0;
-                s.nextLine();
                 if(s.nextLine().equals("Circle")){
-                    shapes[i] = "Circle";
-                    i++;
+                    
                     Circle c = new Circle();
                     c.setRadius(getInt(s.nextLine()));
                     c.setCenterX(getInt(s.nextLine()));
                     c.setCenterY(getInt(s.nextLine()));
                     String[] rgb = s.nextLine().split(",");
                     Color color = Color.rgb(getInt(rgb[0]), getInt(rgb[1]), getInt(rgb[2]));
-                    c.setFill(color);      
+                    c.setFill(color);     
+                    shapes[0] = c;
                 }
                 
                 else if(s.nextLine().equals("Rect")){
-                    shapes[i] = "Rect";
-                    i++;
+
                     
                     Rectangle r = new Rectangle();
                     r.setHeight(getInt(s.nextLine()));
@@ -66,14 +64,6 @@ public class AnimationPlayer {
                     r.setX(getInt(s.nextLine()));
                     r.setY(getInt(s.nextLine()));
                     r.setStrokeWidth(getInt(s.nextLine()));
-                }
-                
-                else if(s.nextLine().equals("Line")){
-                    shapes[i] = "Line";
-                    i++;
-                    
-                    Line l = new Line();
-                   
                 }
             }
         }
@@ -83,10 +73,69 @@ public class AnimationPlayer {
     }
     
     
+    static public void loadAnimationFromFile(String file){
+        try{
+            File f = new File(file);
+            Scanner s = new Scanner(f);
+            while(s.hasNextLine()){
+
+//                this.frames = getInt(s.nextLine());
+//                this.speed = getInt(s.nextLine());
+//                this.elements = getInt(s.nextLine());
+//                
+                int i = 0;
+                s.nextLine();
+                if(s.nextLine().equals("Circle")){
+                    
+                    
+                    Circle c = new Circle();
+                    c.setRadius(getInt(s.nextLine()));
+                    c.setCenterX(getInt(s.nextLine()));
+                    c.setCenterY(getInt(s.nextLine()));
+                    String[] rgb = s.nextLine().split(",");
+                    Color color = Color.rgb(getInt(rgb[0]), getInt(rgb[1]), getInt(rgb[2]));
+                    c.setFill(color);      
+                    shapes[i] = c;
+                    i++;
+                }
+                
+                else if(s.nextLine().equals("Rect")){
+
+                    
+                    Rectangle r = new Rectangle();
+                    r.setHeight(getInt(s.nextLine()));
+                    r.setWidth(getInt(s.nextLine()));
+                    r.setX(getInt(s.nextLine()));
+                    r.setY(getInt(s.nextLine()));
+                    r.setStrokeWidth(getInt(s.nextLine()));
+                    
+                    shapes[i] = r;
+                    i++;
+                }
+                
+                /**
+                else if(s.nextLine().equals("Line")){
+                    shapes[i] = "Line";
+                    i++;
+                    
+                    Line l = new Line();
+                   
+                }
+                 **/
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+    }
     
-    public int getInt(String s){
+    
+    
+    static public int getInt(String s){
         int r = Integer.parseInt(s);
         return r;
     }
+    
+    
 
 }
