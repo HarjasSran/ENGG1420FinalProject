304d92759095f3806793a9a7b6b6517ba71fd1d9
harja <harja@192.168.5.13>
2022-04-02 01:31:10 -0400

    new main method, just making small change ot make sure we have it in place

--- ENGG1420FinalProject/src/engg1420finalproject/ENGG1420FinalProject.java
+++ ENGG1420FinalProject/src/engg1420finalproject/ENGG1420FinalProject.java
@@ -30,54 +30,51 @@ import javafx.animation.AnimationTimer;
  */
 public class ENGG1420FinalProject extends Application {
     
-        public Circle createCircle(int[] circleData){
-            Circle circle = new Circle();
-            circle.setCenterX(circleData[0]);
-            circle.setCenterY(circleData[1]);
-            circle.setRadius(circleData[2]);
-            circle.setFill(Color.BLUE);
-            circle.setStroke(Color.BLACK);
-            circle.setStrokeWidth(10);
-            return circle;
-        }
+//        public Circle createCircle(int[] circleData){
+//            Circle circle = new Circle();
+//            circle.setCenterX(circleData[0]);
+//            circle.setCenterY(circleData[1]);
+//            circle.setRadius(circleData[2]);
+//            circle.setFill(Color.BLUE);
+//            circle.setStroke(Color.BLACK);
+//            circle.setStrokeWidth(10);
+//            return circle;
+//        }
         
-        public Rectangle createRect(int[] rectData){
-            Rectangle rect = new Rectangle();
-            rect.setX(rectData[0]);
-            rect.setY(rectData[1]);
-            rect.setWidth(rectData[2]);
-            rect.setHeight(rectData[3]);
-            rect.setFill(Color.RED);
-            rect.setStroke(Color.WHITE);
-            rect.setStrokeWidth(10);
-            
-            return rect;
-        }
-        
-        public Line createLine(int[] lineData){
-            Line line = new Line();
-            line.setStartX(lineData[0]);
-            line.setStartY(lineData[1]);
-            line.setEndX(lineData[2]);
-            line.setEndY(lineData[3]);
-            line.setFill(Color.BLACK);
-            line.setStrokeWidth(10);
-            
-            return line;
-        }
+//        public Rectangle createRect(int[] rectData){
+//            Rectangle rect = new Rectangle();
+//            rect.setX(rectData[0]);
+//            rect.setY(rectData[1]);
+//            rect.setWidth(rectData[2]);
+//            rect.setHeight(rectData[3]);
+//            rect.setFill(Color.RED);
+//            rect.setStroke(Color.WHITE);
+//            rect.setStrokeWidth(10);
+//            
+//            return rect;
+//        }
+//        
+//        public Line createLine(int[] lineData){
+//            Line line = new Line();
+//            line.setStartX(lineData[0]);
+//            line.setStartY(lineData[1]);
+//            line.setEndX(lineData[2]);
+//            line.setEndY(lineData[3]);
+//            line.setFill(Color.BLACK);
+//            line.setStrokeWidth(10);
+//            
+//            return line;
+//        }
         
         
         
     @Override
     public void start(Stage stage) {
         
-//        int[] circleData =  {200, 150, 50};
-//        int[] rectData = {200, 150, 100, 100};
-//        int[] lineData = {200, 150, 500, 150};
         
         stage.setTitle("Testing Circle");
         
-        AnimationPlayer.loadFile("src\\engg1420finalproject\\Test.txt");
+        AnimationPlayer.loadAnimationFromFile("src\\engg1420finalproject\\Test.txt");
 
         Shape circle = AnimationPlayer.shapes[0];
 //        Circle circle = createCircle(circleData);
