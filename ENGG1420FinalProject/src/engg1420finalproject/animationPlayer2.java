ba6e04a52b072f90dcae243234b214ef890eb71c
harja <harja@192.168.5.13>
2022-04-03 22:30:53 -0400

    done i think

--- ENGG1420FinalProject/src/engg1420finalproject/animationPlayer2.java
+++ ENGG1420FinalProject/src/engg1420finalproject/animationPlayer2.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Harjas Sran, Samy Rashad, Affan Ahmed, Sayansh Verma
+Final Project
+ENGG 1420
+Animation Player
+Animation Player class which reads file and runs animation using javafx
  */
 package engg1420finalproject;
 
